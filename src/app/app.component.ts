86dd88d98428f937547a97ac8f929071568af850
João Luiz <joaoluizmartins6@gmail.com>
2023-05-16 11:29:49 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Tarefa } from "./tarefa";
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TODOapp';
+
+  arrayDeTarefas: Tarefa[] = [];
+  apiURL: string;
+  usuarioLogado = false;
+  tokenJWT = '{ "token":""}';
+  constructor(private http: HttpClient) {
+    this.apiURL = 'http://localhost:3000';
+
+    this.READ_tarefas();
+  }
+  CREATE_tarefa(descricaoNovaTarefa: string) {
+    var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
+    this.http.post<Tarefa>(`${this.apiURL}/api/post`, novaTarefa).subscribe(
+      resultado => { console.log(resultado); this.READ_tarefas(); });
+
+  }
+
+  DELETE_tarefa(tarefaAserRemovida: Tarefa) {
+    var indice = this.arrayDeTarefas.indexOf(tarefaAserRemovida);
+    var id = this.arrayDeTarefas[indice]._id;
+    this.http.delete<Tarefa>(`${this.apiURL}/api/delete/${id}`).subscribe(
+      resultado => { console.log(resultado); this.READ_tarefas(); });
+  }
+
+  READ_tarefas() {
+    const idToken = new HttpHeaders().set("id-token", JSON.parse(this.tokenJWT).token);
+    this.http.get<Tarefa[]>(`${this.apiURL}/api/getAll`, { 'headers': idToken }).subscribe(
+      (resultado) => { this.arrayDeTarefas = resultado; this.usuarioLogado = true },
+      (error) => { this.usuarioLogado = false }
+    )
+  }
+
+
+  // READ_tarefas() {
+  //   this.http.get<Tarefa[]>(`${this.apiURL}/api/getAll`).subscribe(
+  //     resultado => this.arrayDeTarefas = resultado);
+  // }
+
+
+  // Feito dia 18.04
+  UPDATE_tarefa(tarefaAserModificada: Tarefa) {
+    var indice = this.arrayDeTarefas.indexOf(tarefaAserModificada);
+    var id = this.arrayDeTarefas[indice]._id;
+    this.http.patch<Tarefa>(`${this.apiURL}/api/update/${id}`,
+      tarefaAserModificada).subscribe(
+        resultado => { console.log(resultado); this.READ_tarefas(); });
+  }
+
+  login(username: string, password: string) {
+    var credenciais = { "nome": username, "senha": password }
+    this.http.post(`${this.apiURL}/api/login`, credenciais).subscribe(resultado => {
+      this.tokenJWT = JSON.stringify(resultado);
+      this.READ_tarefas();;
+    });
+  }
+
 }
